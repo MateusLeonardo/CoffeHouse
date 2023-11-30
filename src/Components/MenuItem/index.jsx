6cdf7427c8a9438038224c2416f7670a7184610b
MateusLeonardo <mateus_leonardo1997@hotmail.com>
2023-11-30 00:41:41 -0300

    adicionando componente de menuitem

---
+++ src/Components/MenuItem/index.jsx
@@ -0,0 +1,22 @@
+import { FaStar } from "react-icons/fa6";
+import styles from "./styles.module.scss";
+
+export function MenuItem({ response }) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.itemImg}>
+        <span>
+          {response.itemAvaliacao} <FaStar color="#fd9d08" />
+        </span>
+        <img src={response.itemImagem} alt="" />
+      </div>
+      <div className={styles.itemContent}>
+        <h3>
+          {response.itemNome} <span>{response.itemPreco}</span>
+        </h3>
+        <p>{response.itemDescricao}</p>
+      </div>
+      <button>Pedir Agora</button>
+    </div>
+  );
+}
