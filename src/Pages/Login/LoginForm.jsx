e0cc6b3b4c013467d603b4a38b4400a7a281df99
MateusLeonardo <mateus_leonardo1997@hotmail.com>
2023-12-06 00:51:41 -0300

    removendo miragejs

--- src/Pages/Login/LoginForm.jsx
+++ src/Pages/Login/LoginForm.jsx
@@ -1,66 +1,27 @@
 import { Link, useNavigate } from "react-router-dom";
 import styles from "./LoginForm.module.scss";
 import { useEffect, useState } from "react";
-import axios from "axios";
 
 export function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(false);
-
-  const navigate = useNavigate();
-
-  const handleLogin = async (event) => {
-    event.preventDefault();
-
-    try {
-      const response = await axios.post("/api/login", {
-        email: email,
-        password: password,
-      });
-
-      if (response.status === 200) {
-        console.log("Login successful");
-        navigate("/dashboard");
-      } else {
-        setError(true);
-        console.log("Error login");
-      }
-    } catch (error) {
-      setError(true);
-    }
-  };
-
-  useEffect(() => {
-    let timer;
-    if (error) {
-      timer = setTimeout(() => setError(false), 3000);
-    }
-  }, [error]);
 
   return (
     <div className={styles.container}>
       <h1 className={styles.login}>Login</h1>
 
-      <form onSubmit={handleLogin} className={styles.form}>
+      <form className={styles.form}>
         <label htmlFor="email">Email</label>
         <input
           type="email"
           id="email"
-          value={email}
-          onChange={({ target }) => setEmail(target.value)}
           required
         />
         <label htmlFor="password">Senha</label>
         <input
           type="password"
           id="password"
-          value={password}
-          onChange={({ target }) => setPassword(target.value)}
           required
         />
         <button type="submit">Entrar</button>
-        {error && <p>Usuário ou senha inválidos</p>}
       </form>
 
       <Link to="perdeu" className={styles.perdeu}>
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
-import { makeServer } from './api/mirageServer';
-
-// Inicialize o MirageJS somente em ambiente de desenvolvimento
-if (process.env.NODE_ENV === 'development') {
-  makeServer({ environment: 'development' });
-}
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
