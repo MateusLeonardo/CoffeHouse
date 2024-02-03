dfa600ad07c5a11af35c365042aed0e13240d327
mateus leonardo <mateus_leonardo1997@hotmail.com>
2024-02-03 17:04:55 -0300

    arrumando scroll para seção

--- src/Components/SectionTitle/index.jsx
+++ src/Components/SectionTitle/index.jsx
@@ -3,8 +3,8 @@ import styles from './styles.module.scss';
 
 export const SectionTitle = React.forwardRef(({ title, color, id }, ref) => {
   return (
-    <div className={styles.container} id={id} ref={ref}>
-      <h1 className={styles.title} style={{ color: color }}>
+    <div className={styles.container} id={id} >
+      <h1 className={styles.title} style={{ color: color }} ref={ref}>
         {title}
       </h1>
     </div>
